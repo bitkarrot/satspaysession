c50db32663be34f7e88c214b6dd8b7e587a47011
Bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2023-06-07 21:40:11 -0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,6 +2,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def coindesk_btc_fiat(symbol):
+    # batch the requests together via asyncio or multiprocessing
+    setlocale(LC_NUMERIC, '')
+    url = f'https://api.coindesk.com/v1/bpi/currentprice/{symbol}.json'
+    response = requests.get(url)
+    ticker = response.json()
+    time = ticker["time"]['updated']
+    rate = ticker['bpi'][symbol]['rate']
+    parsed_rate = atof(rate)
+    return time, parsed_rate
+
+def get_sats_amt(amount, fiat):
+   time, rate = coindesk_btc_fiat(fiat)
+   btcfiat = "%.2f" % rate
+   moscowtime = int(100000000/float(btcfiat))
+   satstotal =  float(amount * moscowtime)
+   return satstotal
+
 @app.route('/')
 def home():
     return 'SatsPay Session!'
@@ -12,5 +30,11 @@ def about():
 
 @app.route('/<fiat>')
 def dynamic_endpoint(fiat):
-    # Access the value of fiat parameter
-    return f"Endpoint for {fiat}"
+        amount = request.args.get('amount')  
+    if amount:
+        content =  f"Endpoint for {fiat}, The amount is: {amount}"
+        sats = get_sats_amt(amount, fiat)
+        content += f"Sats amount: {sats}"
+        return content
+    else:
+        return f"Endpoint for {fiat}, No amount provided."
