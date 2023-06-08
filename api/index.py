99a79ff94cb132bf2c7e68c12deb45431ca83c7b
Bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2023-06-07 20:46:51 -0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return 'SatsPay Session!'
 
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/<fiat>')
+def dynamic_endpoint(fiat):
+    # Access the value of fiat parameter
+    return f"Endpoint for {fiat}"
